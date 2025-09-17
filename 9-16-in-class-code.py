c1bc20daea858cc79c00b1b674d28345b4a19405
Charlie Norris <charlienorris333@gmail.com>
2025-09-17 08:28:28 -0400

    Add files via upload

---
+++ 9-16-in-class-code.py
@@ -0,0 +1,18 @@
+
+
+def main():
+        numbers = list()
+        names = ["Mathhew", "Suzy", "Jean", "Read"]
+                #    0         1       2       3
+
+        numbers.append(5)
+        numbers.append(78)
+        numbers.append(98)
+        numbers.append(1)
+
+        numbers.sort()
+
+        print(numbers)
+
+        print(names[0][0])     #1st in list and first letter of that
+        print(names)
